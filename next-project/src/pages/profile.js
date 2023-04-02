2e4f62a0ef40090ddaf0f631bb4570aa8eb9b6c2
Tahir Hossain <tahirh9903@gmail.com>
2023-04-02 19:14:52 -0400

    Adjusted Profile Page

--- next-project/src/pages/profile.js
+++ next-project/src/pages/profile.js
@@ -11,23 +11,26 @@ export default function profile(){
     <div className={styles.back}><Link href='/'>Back</Link> </div>
     
     <main className={styles.profileMain}> 
-      <Image
+      <div className ={styles.profilePicture}><Image
           src="/silhouette.png"
           alt="13"
           width={160}
           height={160}
           priority
-        />
+        /></div>
+        <div className={styles.profileBio}>
       <form>
         <label for="name">Enter your name: </label>
+        <br /><br />
         <input type="text" name="name" id="name" required/>
         <br /><br />
         <label for="bio">Enter your bio here: </label>
+        <br /><br />
         <textarea name="bio" id="bio" col="30" rows="10" placeHolder="About you..."/>
         <br /> <br />
         <input type="submit" value="Submit" />
       </form>
-
+    </div>
     </main>
     
     </>
