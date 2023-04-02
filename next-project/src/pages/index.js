02a9368ed5810c3c83716bf09b97ecbd6ecdde97
vadim <symphatixe@gmail.com>
2023-04-02 15:40:24 -0400

    Finalizing landing page links
    We only need 3 links, so I deleted the 4th, since having the generate report function on the landing page without being logged in is useless.

--- next-project/src/pages/index.js
+++ next-project/src/pages/index.js
@@ -2,7 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@component/styles/Home.module.css'
-import Link from 'next/Link';
+import Link from 'next/link';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -77,28 +77,13 @@ export default function Home() {
             rel="noopener noreferrer"
           >
             <h2 className={inter.className}>
-              Templates <span>-&gt;</span>
+              Enter Schedule <span>-&gt;</span>
             </h2>
             <p className={inter.className}>
-              Discover and deploy boilerplate example Next.js&nbsp;projects.
+              Enter your schedule here! 
             </p>
           </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Deploy <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Instantly deploy your Next.js site to a shareable URL
-              with&nbsp;Vercel.
-            </p>
-          </a>
-        </div>
+          </div>
       </main>
     </>
   )
