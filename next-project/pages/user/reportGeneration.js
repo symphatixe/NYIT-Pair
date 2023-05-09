4c1ed622db397b51bc43b56535a376ca398097dc
tahirh9903 <tahirh9903@gmail.com>
2023-05-09 17:59:45 -0400

    Fixed spelling error + a slight resize

--- next-project/pages/user/reportGeneration.js
+++ next-project/pages/user/reportGeneration.js
@@ -11,7 +11,7 @@ export default function ReportGeneration(){
         <>
         <div className = {styles.back}><Link href = '/user/main'>Back</Link> </div>
         <div className = {styles.gridSchedule}>
-        <label htmlFor = "week">Monday&nbsp; Tueday&nbsp; Wednesday&nbsp; Thursday&nbsp; Friday&nbsp; Saturday&nbsp; Sunday</label>
+        <label htmlFor = "week">Monday&nbsp; Tuesday&nbsp; Wednesday&nbsp; Thursday&nbsp; Friday&nbsp; Saturday&nbsp; Sunday</label>
         </div>
         <div className = {styles.gridScheduleTimes}>
         <label htmlFor = "time">8AM <br/><br/>10AM <br/><br/>12PM <br/><br/>2PM <br/><br/>4PM <br/><br/>6PM <br/><br/>8PM <br/><br/>10PM<br/><br/> 12AM</label>
