4adce982235542146e76f20496705b46b2489a1b
Tahir Hossain <tahirh9903@gmail.com>
2023-05-10 23:55:55 -0400

    Created about page

--- next-project/pages/about.js
+++ next-project/pages/about.js
@@ -1,1 +1,49 @@
-//insert code here for about page
+import { Inter } from 'next/font/google'
+import styles from '@component/styles/Home.module.css'
+import Link from 'next/link';
+//import { ChangePageTitle } from '../../server/backend';
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function ReportGeneration(){
+   // ChangePageTitle('about');
+    return (
+        <>
+        <div className = {styles.back}><Link href = '/user/main'>Back</Link> </div>
+        <div className={styles.aboutTitle}>
+        <h1>About Us</h1>
+        </div>
+        <div className = {styles.aboutName}>
+        <h2>Vadim Kan</h2>
+        </div>
+        <div className = {styles.aboutDescription}>
+        <p1>info about vadim here</p1>
+        </div>
+        <div className = {styles.aboutName}>
+        <h2>Tahir Hossain</h2>
+        </div>
+        <div className = {styles.aboutDescription}>
+        <p1>info about tahir here</p1>
+        </div>
+        <div className = {styles.aboutName}>
+        <h2>Timur</h2>
+        </div>
+        <div className = {styles.aboutDescription}>
+        <p1>info about timur here</p1>
+        </div>
+        <div className = {styles.aboutName}>
+        <h2>Ivan Yu</h2>
+        </div>
+        <div className = {styles.aboutDescription}>
+        <p1>info about ivan here</p1>
+        </div>
+        <div className = {styles.aboutName}>
+        <h2>Eddie Ashkenazi</h2>
+        </div>
+        <div className = {styles.aboutDescription}>
+        <p1>info about eddie here</p1>
+        </div>
+        
+        </>
+    )
+}
